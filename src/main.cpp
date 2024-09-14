ec23e89a76bd783ecc3570600bab9a973504ed52
Vasyl Zaichenko <skynet.vasyl@gmail.com>
2024-09-14 08:03:44 +0000

    hello world example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-#include <print>
+#include <fmt/format.h>
+#include <fmt/printf.h>
+
+using namespace std::string_literals;
 
 int main(int argc, char** argv)
 {
-    std::println("Hello, World!");
+    fmt::println("Hello, {}!", "World"s);
     return 0;
 }
